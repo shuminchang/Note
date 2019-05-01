2aeecd4e79a8e3a2ac71da16bfa298a5d9c347a5
Shumin Chang <46363600+shuminchang@users.noreply.github.com>
2019-05-01 23:02:08 +0800

    update

--- visualization.py
+++ visualization.py
@@ -10,11 +10,18 @@
 # skewed data
 # nominal or ordinal, large or small, interval or not, skewed or not - then decide which chart you use
 
+# pandas built-in tools
+# bar chart - order: high count to low count
 reviews['province'].value_counts().head(10).plot.bar()
+# bar chart -  ratio
 (reviews['province'].value_counts().head(10) / len(reviews)).plot.bar()
+# bar chart - sorted(ex. young age to old age)
 reviews['points'].value_counts().sort_index().plot.bar()
+# line chart
 reviews['points'].value_counts().sort_index().plot.line()
+# area chart
 review['points'].value_counts().sort_index().plot.area()
+# histogram - within a range(< 200)
 reviews[reviews['price'] < 200]['price'].plot.hist()
 reviews['price'].plot.hist()
 reviews['points'].plot.hist()
@@ -39,10 +46,9 @@ wine_counts.plot.line()
 # ordinal categorical data, interval data, nominal categorical data
 # outlier, in-betweener
 
-# seaborn: data visualization package
-
 # However, it's a worse choice for ordinal categorical data.
 # A KDE plot expects that if there are 200 wine rated 85 and 400 rated 86, then the values in between, like 85.5, should smooth out to somewhere in between (say, 300). 
+
 # However, if the value in between can't occur (wine ratings of 85.5 are not allowed), then the KDE plot is fitting to something that doesn't exist. 
 # In these cases, use a line chart instead.
 
@@ -50,3 +56,23 @@ wine_counts.plot.line()
 # pandas plotting tools - for initial explorations
 # seaborn - for more sophisticated explorations
 
+# seaborn
+# Countplot - like bar plot
+sns.countplot(reviews['points'])
+# Kernel density estimate(KDE) plot - line plot with outlier
+sns.kdeplot(reviews.query('price < 200').price)
+# 2D KDE plot
+sns.kdeplot(reviews[reviews['price'] < 200].loc[:, ['price', 'points']].dropna().sample(5000))
+# Distplot - histogram with KDE closed
+sns.distplot(reviews['points'], bins=10, kde=False)
+# Scatterplot + hexplot
+sns.jointplot(x='price', y='points', data=reviews[reviews['price'] < 100])
+# Scatterplot + hexplot, Scatterplot as hex form
+sns.jointplot(x='price', y='points', data=reviews[reviews['price'] < 100], kind='hex', 
+              gridsize=20)
+# Boxplot
+df = reviews[reviews.variety.isin(reviews.variety.value_counts().head(5).index)]
+sns.boxplot(x='variety', y='points', data=df)
+# Violinplot
+df = reviews[reviews.variety.isin(reviews.variety.value_counts().head(5).index)]
+sns.violinplot(x='variety', y='points', data=reviews[reviews.variety.isin(reviews.variety.value_counts()[:5].index)])
