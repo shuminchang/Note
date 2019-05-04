dc9afc81cc86841b5029387444c8baf987830082
Shumin Chang <46363600+shuminchang@users.noreply.github.com>
2019-05-04 16:43:39 +0800

    update

--- visualization.py
+++ visualization.py
@@ -76,3 +76,112 @@ sns.boxplot(x='variety', y='points', data=df)
 # Violinplot
 df = reviews[reviews.variety.isin(reviews.variety.value_counts().head(5).index)]
 sns.violinplot(x='variety', y='points', data=reviews[reviews.variety.isin(reviews.variety.value_counts()[:5].index)])
+
+# FacetGrid
+df = footballers[footballers['Position'].isin(['RW', 'LW', 'GK'])]  # select 'RW', 'LW', 'GK' in 'Position' for the first row, each for a column
+df = footballers[footballers['Club'].isin(['Real Madrid CF', 'FC Barcelona', 'Atlético Madrid'])] # select for the second row
+g = sns.FacetGrid(df, col="Position")  # create FacetGrid
+g.map(sns.kdeplot, "Overall")  # make a kdeplot on the FacetGrid, x axis = 'Overall'
+
+g = sns.FacetGrid(df, col="Position", col_wrap=5)  # display 5 column per row
+
+g = sns.FacetGrid(df, row="Position", col="Club", 
+                  row_order=['GK', 'ST'],
+                  col_order=['Atlético Madrid', 'FC Barcelona', 'Real Madrid CF'])  # ordering the row and column
+g.map(sns.violinplot, "Overall")
+
+# Pairplot
+sns.pairplot(footballers[['Overall', 'Potential', 'Value']])
+
+# Multivariate scatter plots, markers for shape, hue for color
+sns.lmplot(x='Value', y='Overall', markers=['o', 'x', '*'], hue='Position',
+           data=footballers.loc[footballers['Position'].isin(['ST', 'RW', 'LW'])],
+           fit_reg=False
+          )
+# Grouped box plot
+f = (footballers
+         .loc[footballers['Position'].isin(['ST', 'GK'])]
+         .loc[:, ['Value', 'Overall', 'Aggression', 'Position']]
+    )
+f = f[f["Overall"] >= 80]
+f = f[f["Overall"] < 85]  # 80 <= f < 85
+f['Aggression'] = f['Aggression'].astype(float)  # ？
+
+sns.boxplot(x="Overall", y="Aggression", hue='Position', data=f)
+
+# Heatmap
+f = (
+    footballers.loc[:, ['Acceleration', 'Aggression', 'Agility', 'Balance', 'Ball control']]
+        .applymap(lambda v: int(v) if str.isdecimal(v) else np.nan)
+        .dropna()
+).corr()
+
+sns.heatmap(f, annot=True) # 'annot' for showing numbers inside the squares
+
+# Parallel Coordinates
+from pandas.plotting import parallel_coordinates
+
+f = (
+    footballers.iloc[:, 12:17]
+        .loc[footballers['Position'].isin(['ST', 'GK'])]
+        .applymap(lambda v: int(v) if str.isdecimal(v) else np.nan)
+        .dropna()
+)
+f['Position'] = footballers['Position']
+f = f.sample(200)
+
+parallel_coordinates(f, 'Position')
+
+# Plotnine
+from plotnine import *
+
+df = top_wines.head(1000).dropna()
+
+(ggplot(df) # passing the data as a parameter
+ + aes(color='points')  # add colors
+ + aes('points', 'price')  # add the variables of interest in aes(aesthetic)
+ + geom_point()  # specify the plot type
+ + stat_smooth()  # add a regression line
+ + facet_wrap('~variety')  # facet with 'variety'. Why '~' ?
+ )  
+  
+(ggplot(df)
+ + geom_point(aes('points', 'price'))
+)
+
+(ggplot(df, aes('points', 'price'))
+ + geom_point()
+)
+
+(ggplot(top_wines)
+     + aes('points')
+     + geom_bar()
+)
+
+(ggplot(top_wines)
+     + aes('points', 'variety')
+     + geom_bin2d(bins=20)
+)
+
+(ggplot(top_wines)
+         + aes('points', 'variety')
+         + geom_bin2d(bins=20)
+         + coord_fixed(ratio=1)
+         + ggtitle("Top Five Most Common Wine Variety Points Awarded")
+)
+
+# Time-series plotting
+shelter_outcomes['date_of_birth'].value_counts().sort_values().plot.line() 
+shelter_outcomes['date_of_birth'].value_counts().resample('Y').sum().plot.line() # resample as 'year'
+stocks['volume'].resample('Y').mean().plot.bar()
+
+# Lag plot
+from pandas.plotting import lag_plot
+lag_plot(stocks['volume'].tail(250))  # correlation with t and t+1
+
+# Autocorrelation plot
+from pandas.plotting import autocorrelation_plot
+autocorrelation_plot(stocks['volume'])
+
+
+
