274f3d319306bb0537153550866cdba8be6257e4
Shumin Chang <46363600+shuminchang@users.noreply.github.com>
2019-08-31 14:26:03 +0800

    Update matplotlib_note.py

--- matplotlib_note.py
+++ matplotlib_note.py
@@ -72,5 +72,80 @@ plt.subplot(133)
 plt.plot(names, values)
 plt.suptitle('Categorical Plotting')
 plt.show()
+# Controling line properties, matplotlib.lines.Line2D
+plt.plot(x, y, linewidth=2.0)
+
+line, = plt.plot(x, y, '-')
+line.set_antialiased(False) # turn off antialiasing
+# setp()
+lines = plt.plot(x1, y1, x2, y2)
+# use keyword args
+plt.setp(lines, color='r', linewidth=2.0)
+# or MATLAB style string value pairs
+plt.setp(lines, 'color', 'r', 'linewidth', 2.0)
+# get a list of settable line properties
+plt.setp(lines)
+
+# Working with multiple figures and axes
+# All plotting commands apply to the current axes.
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+plt.figure()
+plt.subplot(211)
+plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+plt.subplot(212)
+plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+plt.show()
+# Another example
+# The commas in the subplot command are optional if numrows*numcols<10. So subplot(211) is identical to subplot(2, 1, 1).
+# axes(), Axes Demo, Basic Subplot Demo
+plt.figure(1)                # the first figure
+plt.subplot(211)             # the first subplot in the first figure
+plt.plot([1, 2, 3])
+plt.subplot(212)             # the second subplot in the first figure
+plt.plot([4, 5, 6])
+
+plt.figure(2)                # a second figure
+plt.plot([4, 5, 6])          # creates a subplot(111) by default
+
+plt.figure(1)                # figure 1 current; subplot(212) still current
+plt.subplot(211)             # make subplot(211) in figure1 current
+plt.plot([6, 7, 8])          # This will create a new line on the subplot(211)
+plt.title('Easy as 1, 2, 3') # subplot 211 title
+# clf() - clear current figure
+# cla() - clear current axes
+# Artist tutorial
+'''
+the memory required for a figure is not completely released until the figure is explicitly closed with close(). 
+Deleting all references to the figure, and/or using the window manager to kill the window in which the figure 
+appears on the screen, is not enough, because pyplot maintains internal references until close() is called.
+'''
+# Working with text - xlabel(), ylabel(), title(), text(), Text in Matplotlib Plots
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000)
+
+# the histogram of the data
+n, bins, patches = plt.hist(x, 50, density=1, facecolor='g', alpha=0.75)
+
+
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+plt.show()
+
+t = plt.xlabel('my data', fontsize=14, color='red')
+# Using mathematical expressions in text, matplotlib accepts TeX equation expressions in any text expression
+plt.title(r'$\sigma_i=15$')
+# Writing mathematical expressions
+Text rendering With LaTeX
+# Annotating text
 
 
