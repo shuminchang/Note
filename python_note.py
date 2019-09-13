df3bb845f5ed6da97b349b1e8dbe039b598c58d4
Shumin Chang <46363600+shuminchang@users.noreply.github.com>
2019-09-13 17:38:11 +0800

    update python_note.py

--- python_note.py
+++ python_note.py
@@ -4,3 +4,7 @@ a = [1, 2, 3]
 b = [3, 2, 1]
 
 a, b = b, a
+
+'''
+iterable - An object capable of returning its members one at a time
+ex. sequence types - list, str, tuple; non-sequence types - dict, file objects, classes: __iter__(), __getitem__()
